6ddf966763c3a880e89322e4d6682afbbea6e28f
Pedro Hernandez <pherned@gmail.com>
2018-09-06 11:33:39 -0400

    Accessed NYC Open Data API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            locationData: [],
+            vehicleData: [],
+
+        }
+    }
+    
+    componentDidMount = async () => {
+        const nypdApi = await fetch('https://data.cityofnewyork.us/resource/qiz3-axqb.json');
+        const nypdData = await nypdApi.json();
+        console.log (nypdData);
+
+        this.setState({
+            // locationData: nypdData,
+        })
+        // const locData = locationData;
+        // console.log(locData);
+    }
+
+    render() {
+        return(
+        <p>Hello World!</p>
+        )
+    }
+
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
