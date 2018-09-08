019fa9cba8dd7de551c14571ee7017635a51880f
Pedro Hernandez <pherned@gmail.com>
2018-09-08 15:25:43 -0400

    Additional IndicentList tweaks.

--- src/IncidentList/index.js
+++ src/IncidentList/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import moment from "moment";
-import Map from "../Map";
+// import Map from "../Map";
 import MapThumb from "../MapThumb"
 import "./style.css";
 
@@ -16,23 +16,25 @@ class IncidentList extends Component {
                 <div className="incidents_wrapper">
                     {this.props.incidents.map((item, index) => {
                         return (
-                            <div key={item.unique_key}>{moment(item.date).format("dddd, MMMM Do YYYY")} at {moment(item.time, 'hh:mm a').format("hh:mm a")}{this.props.borough && <span className="zip-span"> in zip code {item.zip_code}</span>}
-                            <MapThumb latitude={item.latitude} longitude={item.longitude} />
-                            <Map latitude={item.latitude} longitude={item.longitude} />
-                            {item.number_of_persons_injured > 0 && <p>People hurt: {item.number_of_persons_injured}</p>}
+                            <div key={item.unique_key}>
+                                {(parseInt(item.number_of_persons_injured) === 0 && parseInt(item.number_of_persons_killed) === 0) ? `No one was hurt` : `At least one person was hurt`} {moment(item.date).format("dddd, MMMM Do YYYY")} at {moment(item.time, 'hh:mm a').format("hh:mm a")}{this.props.borough && <span className="zip-span"> in zip code {item.zip_code}</span>}
+                                <p>Click or tap for details.</p>
+                                {/* {item.number_of_persons_injured > 0 && <p>People hurt: {item.number_of_persons_injured}</p>}
                                 {item.number_of_persons_killed > 0 && <p>People killed: {item.number_of_persons_killed}</p>}
                                 {item.number_of_cyclists_injured > 0 && <p>Cyclists hurt: {item.number_of_cyclists_injured}</p>}
                                 {item.number_of_cyclist_killed > 0 && <p>Cyclists killed: {item.number_of_cyclist_killed}</p>}
                                 {item.number_of_pedestrians_injured > 0 && <p>Pedestrians hurt: {item.number_of_pedestrians_injured}</p>}
                                 {item.number_of_pedestrians_killed > 0 && <p>Pedestrians killed: {item.number_of_pedestrians_killed}</p>}
                                 {item.number_of_motorist_injured > 0 && <p>Motorists hurt: {item.number_of_motorist_injured}</p>}
-                                {item.number_of_motorist_killed > 0 && <p>Motorists killed: {item.number_of_motorist_killed}</p>}
+                                {item.number_of_motorist_killed > 0 && <p>Motorists killed: {item.number_of_motorist_killed}</p>} */}
+                                <MapThumb latitude={item.latitude} longitude={item.longitude} />
+                                {/* <Map latitude={item.latitude} longitude={item.longitude} /> */}
                             </div>
                         );
                     })
                     }
                 </div>
-                
+
             </div>
         );
     }
